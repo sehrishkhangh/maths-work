63e624c84652223bdc9f713514e460211144a577
sehrishkhangh <s0331khan@gmail.com>
2019-07-01 00:54:23 +0500

    half diamond shape.

---
+++ halfdiamond.py
@@ -0,0 +1,20 @@
+r=1
+for i in range(7):
+    for l in range (1,7):
+        print('',end='')
+    for j in range(1,r):
+    
+        #print(i,end='')
+        #print(j,end='')
+        print('*',end='')
+    print()
+    r=r+1
+m=6
+for i in range(6):
+    for j  in range(1,m):
+        #for k in range(3):
+        print('*',end='')
+    print()
+    m=m-1
+
+    
