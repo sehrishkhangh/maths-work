ae79305abb3dc43fdf1ad5375e52d7f95a8f22ac
sehrishkhangh <s0331khan@gmail.com>
2019-07-01 20:25:15 +0500

    commiting divisible numbers work.

---
+++ q3.py
@@ -0,0 +1,9 @@
+print('=============================DIVISIBLE NUMBERS=============================')
+print('\n')
+
+n=int(input('Give your first number please! '))
+m=int(input('Give your second number please! '))
+if n/m==0:
+    print('You have two divisible numbers! ')
+else:
+    print('You have undivisible numbers! ')
