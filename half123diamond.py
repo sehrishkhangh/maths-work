02fa208ec6759b46ea3c5bde6702fea571f86785
sehrishkhangh <s0331khan@gmail.com>
2019-07-01 14:05:47 +0500

    commiting half diamond pttern for 123.

---
+++ half123diamond.py
@@ -0,0 +1,18 @@
+r=1
+for i in range(7):
+    #for l in range (1,7):
+        #print(end=' ')
+    for j in range(1,r):
+    
+        #print(i,end='')
+        #print(j,end='')
+        print(j,end=' ')
+    print()
+    r=r+1
+m=6
+for i in range(6):
+    for j  in range(1,m):
+        #for k in range(3):
+        print(j,end=' ')
+    print()
+    m=m-1
