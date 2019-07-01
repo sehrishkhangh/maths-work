e889f44193a7ca1c9b2d87d833643c5adb848e44
sehrishkhangh <s0331khan@gmail.com>
2019-07-01 23:11:57 +0500

    commmiting vowel code.

---
+++ q11.py
@@ -0,0 +1,7 @@
+given_word=input('enter your word! ')
+vowels=['a','e','i','o','u']
+for letter in vowels:
+    if given_word==vowels:
+        print('Yes! :) You have a wovel.')
+    else:
+        print('Sorry! :( You dont have wovel..')
