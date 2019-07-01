3e919b477814cf354464e502f35f804fccde1c96
sehrishkhangh <s0331khan@gmail.com>
2019-07-01 15:28:50 +0500

    comiting nature of a number  assignment.

---
+++ q2.py
@@ -0,0 +1,9 @@
+print('=============================THE NUMBER YOU HAVE===================================')
+print('\n')
+n=int(input('Enter your number please! '))
+if n<0:
+    print('It is a negative number.')
+elif n==0:
+    print('You have a zero.')
+else:
+    print('You are dealing with a positive number.')
