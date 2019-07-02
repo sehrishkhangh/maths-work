b5ee3dd4c9cfc46ae07fe1d56a61fe30067af3f9
sehrishkhangh <s0331khan@gmail.com>
2019-07-02 21:17:37 +0500

    COMMIING B.M.I FORMULA.

---
+++ q22.py
@@ -0,0 +1,10 @@
+print('============================= B.M.I ====================================')
+print('\n')
+weight=int(input('Your weight in Kg! '))
+height=int(input('Your height in meter! '))
+b_m_i=weight/(height*height)
+print('B.M.I= ',b_m_i)
+print('\n')
+print('================================   THE END   ===========================')
+
+
