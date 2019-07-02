bc9bed42ef5050ce851d8346859842c8e9bfd302
sehrishkhangh <s0331khan@gmail.com>
2019-07-02 20:00:25 +0500

    commiting height convertor code.

---
+++ q17.py
@@ -0,0 +1,14 @@
+#1ft=30.48cm, 1inch=2.54cm
+print('============================HEIGHT CALCULATING===========================')
+print('\n')
+
+ft_cm=30.48
+inch_cm=2.54
+height_ft=int(input('Enter your height in feet, you want to calculate: '))
+height_inch=int(input('Enter your height in inch, you want to calculate: '))
+height_ft_cm=int(height_ft*ft_cm)
+height_inch_cm=int(height_inch*inch_cm)
+print('Your feet height in cm is= ',height_ft_cm)
+print('Your inches height in cm is= ',height_inch_cm)
+print('\n')
+print('================================   THE END   ===========================')
