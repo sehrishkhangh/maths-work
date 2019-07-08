ef625395151482513a92a5d9725c48b377992d1a
sehrishkhangh <s0331khan@gmail.com>
2019-07-08 13:35:53 +0500

    commiting q30

---
+++ q30.py
@@ -0,0 +1,10 @@
+print('======================= PARTICUAR ALPHABET IN STRING =====================')
+print('\n')
+
+string1=input('Insert your string= ')
+string2=input('Insert your favoritte alphabet= ')
+counter=string1.count(string2)
+print(counter)
+print('================================ THE END ====================================')
+print('\n')
+
