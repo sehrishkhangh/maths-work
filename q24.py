b655fc62da89b9b825423d32b5098e7994f350e4
sehrishkhangh <s0331khan@gmail.com>
2019-07-10 15:32:17 +0500

    COMMITHING Q#24

---
+++ q24.py
@@ -0,0 +1,10 @@
+print('======================== THE FACTORIAL SUM ===========================')
+print('\n')
+your_number=int(input('Enter your number= '))
+the_sum=your_number*(your_number+1)/2
+print(the_sum)
+print('\n')
+print('======================== THE END ===========================')
+
+
+
