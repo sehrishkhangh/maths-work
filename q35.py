2db0473ab7dba545fbe8fe9ea08ee763fed65b4b
sehrishkhangh <s0331khan@gmail.com>
2019-07-12 19:56:26 +0500

    COMMITING Q#35

---
+++ q35.py
@@ -0,0 +1,9 @@
+print('=========================== NUMBER OF NOTES IN AN AMOUNT =============================')
+print('\n')
+x=int(input('Amount= '))
+y=int(input('Notes= '))
+for i in range(1):
+    z=x/y
+print('Notes of ',y,'in ',x,'= ',int(z))
+print('\n')
+print('====================================== THE END =======================================')
