79730c724471ea46d42cac9f9e00155111fef28c
sehrishkhangh <s0331khan@gmail.com>
2019-07-12 22:44:15 +0500

    COMMITING PALINDROME.

---
+++ q#36.py
@@ -0,0 +1,12 @@
+print('=================================== PALINDROME ===================================')
+print('\n')
+x=input('String! ')
+if x[::-1]==x:
+    print('It is palindrome:)')
+else:
+    print('Its not palindromme:( ')
+print('\n')
+print('===================================== THE END ====================================')
+
+
+
